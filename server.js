9c60e739ad9eb7ae33acb3d75c79c1a2780a7605
Joonas Mielonen <Joonas.mielonen@metropolia.fi>
2021-03-23 11:28:03 +0200

    update

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(express.urlencoded({extended: false}));
 
 app.use('/cat', require('./routes'));
 
+app.get('/', (req, res) => {
+  console.log('get /');
+  res.send('You can get cats from here');
+});
+
 db.on('connected', () => {
   console.log('connected to port 3000');
   app.listen(3000);
